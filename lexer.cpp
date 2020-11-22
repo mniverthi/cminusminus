2200a02120c528e457d3aa49d4e14d40b08a1e31
Manoj Niverthi <manojniverthi@gmail.com>
2020-11-22 16:35:12 -0500

    built outline and token types

---
+++ lexer.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include "tokentype.h"
+using namespace std;
+
+class Lexer {
+    private:
+        int currentPosition;
+        int currentCharacter;
+    public:
+        Lexer() {
+            currentPosition = 0;
+            cout << "Hello world" << endl;
+        }
+        void nextCharacter() {
+            
+        }
+        void skipComment() {
+
+        }
+        string getToken() {
+
+        }
+
+};
